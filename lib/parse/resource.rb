a7802b6e32a74eebb0b0877ba7268e2d87f49623
Zhi-Qiang Lei <zhiqiang.lei@gmail.com>
2012-07-01 13:18:33 +0800

    enrich error information

--- lib/parse/resource.rb
+++ lib/parse/resource.rb
@@ -1,5 +1,42 @@
 module DataMapper
   module Parse
+    # Read the body to get detail of Parse error
+    class Connection < ::Nestful::Connection
+      def handle_response(response)
+        response.read_body if (400...500).include? response.code.to_i
+        super
+      end
+    end
+
+    ::Nestful::Request.class_eval do
+      def connection
+        conn              = Connection.new(uri, format)
+        conn.proxy        = proxy if proxy
+        conn.user         = user if user
+        conn.password     = password if password
+        conn.auth_type    = auth_type if auth_type
+        conn.timeout      = timeout if timeout
+        conn.ssl_options  = ssl_options if ssl_options
+        conn
+      end
+    end # This is a workaround. Mixin for Connection does not work somehow.
+
+    module ParseError
+      def to_s
+        error = JSON.parse(response.body)["error"]
+        super + "  Response Parse Error = #{error}"
+      end
+    end
+
+    ::Nestful::BadRequest.send          :include, ParseError
+    ::Nestful::UnauthorizedAccess.send  :include, ParseError
+    ::Nestful::ForbiddenAccess.send     :include, ParseError
+    ::Nestful::ResourceNotFound.send    :include, ParseError
+    ::Nestful::MethodNotAllowed.send    :include, ParseError
+    ::Nestful::ResourceConflict.send    :include, ParseError
+    ::Nestful::ResourceGone.send        :include, ParseError
+    ::Nestful::ResourceInvalid.send     :include, ParseError
+
     class Resource < ::Nestful::Resource
       attr_reader :options
 
